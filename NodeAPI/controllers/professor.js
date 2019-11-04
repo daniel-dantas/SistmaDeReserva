13de0a57bade4e5a149d6c8c186b5c1ff8fbd496
XSrDanDanX <ddipaumirim@gmail.com>
2019-11-03 23:47:26 -0300

    Controlador de professores pronto

--- NodeAPI/controllers/professor.js
+++ NodeAPI/controllers/professor.js
@@ -3,22 +3,20 @@ const Professor = require('../models/Professor')
 const Usuario = require('../models/Usuario')
 const router = express.Router()
 
-router.get('/', (req,res)=>{
-    res.send('Professores')
-})
+
 
 router.post('/search',async (req,res)=>{
-     await Usuario.findOne({where: {matricula: req.body.matricula}, attributes: ['matricula','nome', 'email']}).then(user => {
-        Professor.findOne({where: {matricula: user.matricula}, attributes: ['disciplinas']}).then(prof => {
+     await Professor.findOne({where: {matricula: req.body.matricula}, attributes: ['matricula','disciplinas']}).then(prof => {
+        Usuario.findOne({where: {matricula: req.body.matricula}, attributes: ['nome', 'email']}).then(user => {
             return res.send({user: user, disciplinas: prof.disciplinas})
-        })
-    })
+        }).catch(()=>{return res.send(null)})
+    }).catch(()=>{return res.send(null)})
 })
 
 router.post('/create', async (req,res)=>{
-    try{
+    
         var matricula = req.body.matricula
-        Usuario.create({
+        await Usuario.create({
             matricula: matricula,
             nome: req.body.nome,
             senha: req.body.senha,
@@ -30,19 +28,44 @@ router.post('/create', async (req,res)=>{
         }).catch((error)=>{
             return res.send('Nao deu certo: '+error)
         })
-    }catch(err){
-        res.send('erro')
-    }
-    
-    
     
     
     
 })
 
-router.get('/auth', (req,res)=>{
-    return res.send('Autenticando')
+router.post('/delete', async (req,res)=>{
+    await Professor.destroy({
+        where: {matricula: req.body.matricula}
+    }).then(()=>{
+        Usuario.destroy({
+            where: {matricula: req.body.matricula}
+        }).then(()=>{
+            return res.send(true)
+        })
+    }).catch((error)=>{
+        return res.send(false)
+    })
 })
 
+router.post('/update', (req,res)=>{
+    Usuario.update({nome: req.body.nome, senha: req.body.senha, email: req.body.email}, {where: {matricula: req.body.matricula}}).then((user)=>{
+        Professor.update({disciplinas: req.body.disciplinas}, {where: {matricula: req.body.matricula}}).then((prof)=>{
+            return res.send(user)
+        }).catch(()=>{return res.send(null)})
+    }).catch(()=>{return res.send(null)})
+})
+
+router.post('/auth', (req,res)=>{
+    Professor.findOne({where: {matricula: req.body.matricula}}).then(professor => {
+        Usuario.findOne({where: {matricula: req.body.matricula}, attributes: ['matricula', 'senha', 'nome', 'email']}).then(user=>{
+            if(user.senha == req.body.senha){
+                user.senha = ""
+                return res.send({user: user, disciplinas: professor.disciplinas})
+            }else{
+                return res.send(null)
+            }
+        }).catch(()=>{res.send(null)})
+    }).catch(()=>{res.send(null)})
+})
 
 module.exports = router
